49a19bb08456ea3e0ad5342454a32db837c3f551
SEROSH <janmejayapanda400@gmail.com>
2025-11-14 03:00:20 +0530

    Create object.hpp

---
+++ src/vm/object/object.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "../common/common.hpp"
+
+class VM;
+
+class Obj {
+public:
+    enum class Type { STRING, FUNCTION, CLOSURE, UPVALUE, CLASS, INSTANCE, BOUND_METHOD };
+    Type type;
+    bool marked = false;
+    Obj* next = nullptr;
+
+    explicit Obj(Type t) : type(t) {}
+    virtual ~Obj() = default;
+};
+
+#define AS_OBJ(value)       (std::get<Obj*>(value))
+#define AS_STRING(value)    (AS_OBJ(value)->as<ObjString>())
+#define AS_CSTRING(value)   (AS_STRING(value)->c_str())
+#define AS_FUNCTION(value)  (AS_OBJ(value)->as<ObjFunction>())
+#define AS_CLOSURE(value)   (AS_OBJ(value)->as<ObjClosure>())
+#define AS_CLASS(value)     (AS_OBJ(value)->as<ObjClass>())
+#define AS_INSTANCE(value)  (AS_OBJ(value)->as<ObjInstance>())
+#define AS_BOUND(value)     (AS_OBJ(value)->as<ObjBoundMethod>())
+
+template<typename T>
+inline T* as(Obj* obj) { return dynamic_cast<T*>(obj); }
