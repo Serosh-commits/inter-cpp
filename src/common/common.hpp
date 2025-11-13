67a8bf93d33afbeebc17daac64778611262fdb6a
SEROSH <janmejayapanda400@gmail.com>
2025-11-14 02:53:57 +0530

    Add common.hpp header file with includes and macros

---
+++ src/common/common.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <variant>
+#include <memory>
+#include <unordered_map>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdarg>
+#include <array>
+
+#define DEBUG_PRINT_CODE false
+#define DEBUG_TRACE_EXECUTION false
+#define DEBUG_STRESS_GC false
+#define DEBUG_LOG_GC false
