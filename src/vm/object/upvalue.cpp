079755bce0c0cb81fbc4c5db1a63b9f6de7af209
Serosh <janmejayapanda400@gmail.com>
2026-03-28 15:22:17 +0530

    Upvalues look up now

--- src/vm/object/upvalue.cpp
+++ src/vm/object/upvalue.cpp
@@ -1,1 +1,10 @@
 #include "upvalue.hpp"
+#include "../vm.hpp"
+
+void ObjUpvalue::blacken(VM& vm) {
+    vm.markValue(closed);
+}
+
+std::string ObjUpvalue::toString() const {
+    return "upvalue";
+}
--- src/vm/object/upvalue.hpp
+++ src/vm/object/upvalue.hpp
@@ -9,4 +9,6 @@ public:
     ObjUpvalue* next = nullptr;
 
     ObjUpvalue(Value* slot) : Obj(Type::UPVALUE), location(slot) {}
+    void blacken(VM& vm) override;
+    std::string toString() const override;
 };
